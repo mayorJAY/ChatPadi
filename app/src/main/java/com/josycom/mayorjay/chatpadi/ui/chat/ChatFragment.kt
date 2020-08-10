739e0a4dbd002921e37082b44fad7a2387197902
mayorjay <joseolu4gsm@yahoo.com>
2020-08-10 21:09:00 +0100

    Feat: Setup Chat Fragment and Navigation component

---
+++ app/src/main/java/com/josycom/mayorjay/chatpadi/ui/chat/ChatFragment.kt
@@ -0,0 +1,25 @@
+package com.josycom.mayorjay.chatpadi.ui.chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.josycom.mayorjay.chatpadi.R
+
+class ChatFragment : Fragment() {
+
+    //private val viewModel: ChatViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/josycom/mayorjay/chatpadi/ui/chat/ChatViewModel.kt
@@ -0,0 +1,7 @@
+package com.josycom.mayorjay.chatpadi.ui.chat
+
+import androidx.lifecycle.ViewModel
+
+class ChatViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
