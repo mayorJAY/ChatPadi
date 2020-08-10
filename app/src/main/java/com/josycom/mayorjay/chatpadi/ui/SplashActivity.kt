48be1f4e8b115f748c8a15e7afe5e37cf1a89cab
mayorjay <joseolu4gsm@yahoo.com>
2020-08-10 23:32:44 +0100

    Feat: Setup Home Fragment and layout

--- app/src/main/java/com/josycom/mayorjay/chatpadi/ui/SplashActivity.kt
+++ app/src/main/java/com/josycom/mayorjay/chatpadi/ui/SplashActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.josycom.mayorjay.chatpadi.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
         val handler = Handler()
         handler.postDelayed(this::goToMainActivity, 1500)
---
+++ app/src/main/java/com/josycom/mayorjay/chatpadi/ui/home/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.josycom.mayorjay.chatpadi.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.josycom.mayorjay.chatpadi.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        chat_button.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_chatFragment)
+        }
+    }
+}
